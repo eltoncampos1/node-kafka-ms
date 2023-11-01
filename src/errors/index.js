dcfa63b331034f31de8777e8204696c08b8411e6
Elton Campos <eltoncampos36@gmail.com>
2023-10-31 22:05:51 -0300

    feat: create InvalidLengthError and InvalidFormatError

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,6 +1,20 @@
-export default class InvalidUUIDError extends Error {
+export class InvalidUUIDError extends Error {
   constructor (message) {
     super(message || 'Invalid uuid')
     this.name = 'InvalidUUIDError'
+  }
+}
+
+export class InvalidLengthError extends Error {
+  constructor (message) {
+    super(message || 'Invalid length')
+    this.name = 'InvalidLengthError'
+  }
+}
+
+export class InvalidFormatError extends Error {
+  constructor (message) {
+    super(message || 'Invalid format')
+    this.name = 'InvalidLengthErrorInvalidFormatError'
   }
 }
