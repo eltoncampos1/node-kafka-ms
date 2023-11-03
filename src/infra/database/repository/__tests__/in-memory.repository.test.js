649ec177665b94d68c56f9738fd326623e9f8faa
Elton Campos <eltoncampos36@gmail.com>
2023-11-03 10:56:31 -0300

    test: should delete an item

--- src/infra/database/repository/__tests__/in-memory.repository.test.js
+++ src/infra/database/repository/__tests__/in-memory.repository.test.js
@@ -1,10 +1,20 @@
 import { describe, expect, test } from '@jest/globals'
 import InMemoryRepository from '../in-memory.repository'
 
-describe('In Memory-repository', () => {
-  const makeSut = () => {
-    return new InMemoryRepository()
+const makeSut = () => {
+  return new InMemoryRepository()
+}
+
+const makeSutWithSeeds = (times) => {
+  const repo = makeSut()
+  for (let i = 0; i < times; i++) {
+    repo.create({ id: i, name: 'item ' + i })
   }
+
+  return repo
+}
+
+describe('In Memory-repository', () => {
   test('should create an item', () => {
     const item = { id: 1, name: 'item 1' }
 
@@ -14,5 +24,14 @@ describe('In Memory-repository', () => {
     const result = sut.create(item)
     expect(result).toEqual(item)
     expect(sut.findAll()).toHaveLength(1)
+  })
+
+  test('should delete an item', () => {
+    const sut = makeSutWithSeeds(10)
+    const item = { id: 1, name: 'item 1' }
+
+    expect(sut.findAll()).toHaveLength(10)
+    sut.delete(item.id)
+    expect(sut.findAll()).toHaveLength(9)
   })
 })
